40598268120127e047200f9b34c6718658d8e7b0
Michael <mike.witk@gmail.com>
2019-05-15 18:05:06 +0000

    exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,15 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# david = User.find_by(name: 'David')
+
+@store1 = Store.find_by(id: 1)
+
+puts @store1.inspect
+
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "LighStore Labs"
+@store1.save
+
+puts @store1.inspect
