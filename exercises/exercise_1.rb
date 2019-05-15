f0eae8f00e81f2d4b6edf95d0513e2f9fd173cea
Michael <mike.witk@gmail.com>
2019-05-15 16:56:51 +0000

    Exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,18 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# User.create(name: "David", occupation: "Code Artist")
+
+# create_table :stores do |t|
+#   t.column :name, :string
+#   t.column :annual_revenue, :integer
+#   t.column :mens_apparel, :boolean
+#   t.column :womens_apparel, :boolean
+#   t.timestamps null: false
+
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
